2f483b16dccfb66e7d45bcffd9382a0a499ca50b
Roberta Moschetti <robertamoschetti@MacBook-Air-di-Roberta.local>
2024-12-09 19:12:58 +0100

    Modifica per docker

--- my-app/src/config.ts
+++ my-app/src/config.ts
@@ -1,5 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 export default {
   REPORT_API: `${API_BASE_URL}/api/report`,
   DOWNLOAD_CSV_API: `${API_BASE_URL}/api/download-csv`,
